fa7d65c3377c7e73038006d60903edf5284a4634
MKPJ <bmkpj5@gmail.com>
2026-08-17 19:58:53 +0330

    feat: add union type support for icon prop in reusable Card

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -1,6 +1,8 @@
+import { LucideIcon } from "lucide-react";
+
 export default interface CardProp {
   title: string;
-  icon: string;
+  icon: LucideIcon | string;
   description: string;
   delay: number;
 }
