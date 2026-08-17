6388c525e5a4530489f4b9771fc1329daa8848cd
MKPJ <bmkpj5@gmail.com>
2026-08-17 19:01:56 +0330

    chore: add @ alias in vite config for better import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
