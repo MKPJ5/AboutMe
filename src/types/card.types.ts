f0ce50219d6708778dfd56584dd543bc101515b5
MKPJ <bmkpj5@gmail.com>
2026-08-18 17:32:22 +0330

    refactor:separate cardProp with cardType

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -1,8 +1,15 @@
 import { LucideIcon } from "lucide-react";
 
-export default interface CardProp {
+interface CardType {
   title: string;
   icon: LucideIcon | string;
   description: string;
   delay: number;
 }
+
+interface CardProp extends CardType {
+  card?: CardProp;
+  onCardSelected?: (card: any) => void;
+}
+
+export type { CardProp, CardType };
