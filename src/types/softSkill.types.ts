d363a21a9ec97ec2d2865fd20a07ece598b5336e
MKPJ <bmkpj5@gmail.com>
2026-08-18 17:28:35 +0330

    feat: add separate SoftSkillType for better control in skillDetailWindow

---
+++ src/types/softSkill.types.ts
@@ -0,0 +1,11 @@
+import { LucideIcon } from "lucide-react";
+
+interface SoftSkillType {
+  title: string;
+  icon: LucideIcon;
+  description: string;
+  delay: number;
+  moreDetails: string;
+}
+
+export type { SoftSkillType };
