2f3b53c959b1b28d047399b73615d76b68759054
MKPJ <bmkpj5@gmail.com>
2026-08-24 19:55:19 +0330

    feat : add dayRoutineInfo to use it in contact page

---
+++ src/data/dayRoutin/DayRoutineInfo.ts
@@ -0,0 +1,84 @@
+import { JourneyProp } from "@/types/JourneyItemTypes/JourneyItem.types";
+
+const DayRoutine: JourneyProp[] = [
+  {
+    title: "The Alarm Rings",
+    date: "3:00 AM",
+    description: "A new day starts. A new chance to build.",
+  },
+  {
+    title: "Running",
+    date: "3:10 AM",
+    description:
+      "I go outside and run. The city is quiet and air is cleaner than other times. Everyone's asleep. That run wakes my body up and makes me feel fresh. 🏃‍♂️",
+  },
+  {
+    title: "Cold Shower",
+    date: "3:40 AM",
+    description:
+      "Back home. A cold shower is waiting for me. It's part of every day routine and make me energetic . 🚿",
+  },
+  {
+    title: "Writing the Day Plan",
+    date: "3:55 AM",
+    description:
+      "After the shower, I write my plan for the day. While writing it, I know exactly what I have to do. It keeps me focused. ✍️",
+  },
+  {
+    title: "Deep Work",
+    date: "4:10 AM",
+    description: "I start working and check how many tasks I can get done.",
+  },
+  {
+    title: "Bakery Work",
+    date: "5:30 AM",
+    description:
+      "I'm a developer, but I don't have a developer job yet. So I work at a bakery in the mornings. Survival first. Craft after. 🥖",
+  },
+  {
+    title: "Midday Reset",
+    date: "12:00 PM ",
+    description:
+      "After hours of being awake and working, my body needs a reset. I sleep for two hours.💤",
+  },
+  {
+    title: "Creating Again",
+    date: "2:00 PM",
+    description:
+      "I'm back with more energy. What I create depends on the daily plan — it could be an article, a website, or learning something new. ",
+  },
+  {
+    title: "English Class",
+    date: "7:00 PM",
+    description: "My English class. my favorite language.",
+  },
+  {
+    title: "Second Training",
+    date: "9:00 PM",
+    description:
+      "Second part of my workout. Every day, I train a specific part of my body. No coach No gym — I wrote the plan myself and train in the home. 💪",
+  },
+  {
+    title: "Vocabulary Time",
+    date: "9:45 PM",
+    description: "Time to memorize new English words or review old ones.📓",
+  },
+  {
+    title: "Last Checking",
+    date: "10:00",
+    description: "The day almost done I check my daily plan to unfulfilled tasks.✔️",
+  },
+  {
+    title: "Reading",
+    date: "11:00 PM",
+    description: "A useful book every single night. My mind needs fuel, just like my body. 📚",
+  },
+  {
+    title: "End of Day",
+    date: "11:20 PM",
+    description:
+      "It's extra time I use it to rest and sleep. at least i sleep for 5:30 hours per day.🛏️",
+  },
+];
+
+export { DayRoutine };
