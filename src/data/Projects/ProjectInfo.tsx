a038e202919a50d6b82292eded1242f736bd422c
MKPJ <bmkpj5@gmail.com>
2026-08-31 05:05:49 +0330

    feat: add projectsInfo

---
+++ src/data/Projects/ProjectInfo.tsx
@@ -0,0 +1,11 @@
+import { ProjectType } from "@/types/ProjectTypes/project.types";
+export const ProjectsInfo: ProjectType[] = [
+  {
+    id: 1,
+    title: "Todo SPA",
+    description: "A simple Todo Application with feature of saving tasks in local storage.",
+    githubUrl: "https://github.com/MKPJ5/Todo-App",
+    vercelUrl: "https://ecommerce.vercel.app",
+    techStack: ["React", "TypeScript", "Zod", "Tailwind CSS"],
+  },
+];
